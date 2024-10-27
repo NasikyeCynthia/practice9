60430d42922faf7faf58f52f1d6508414031934a
NasikyeCynthia <cynthianasikye@gmail.com>
2024-10-27 19:56:19 +0300

    Add files via upload

---
+++ Question1.py
@@ -0,0 +1,7 @@
+fees = 1500000
+years = int(input("How many years is your course?: "))
+sem = years * 2
+total = sem * fees
+print(f"There are {sem} semsters for a {years}-year program.")
+print(f"Tuition fees for each semester is shs. {fees}")
+print(f"Your total tuition for your course is shs. {total}")
